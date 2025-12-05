cd067f6f9e95dfd0c06592c0ab2ffcc9d178cee4
Sebastian Ramirez <se.ramirez4155@gmail.com>
2025-12-05 15:24:19 -0800

    calcuated Age with entered birthday and sorted bday

--- src/app.jsx
+++ src/app.jsx
@@ -4,27 +4,59 @@ export default function Board(){
     // create a piece of state to store the name
     const [name, setName] = useState("")
     const [birthday, setBirthday] = useState("")
-    const [age, setAge] = useState("")
-  // create a function to handle the name change
+    
+
+
+    const [people, setPeople] = useState([])
+      // create a function to handle the name change
     function handleNameChange(e){
         setName(e.target.value)
     }
     function handleBirthdayChange(e){
         setBirthday(e.target.value)
     }
-    function handleAgeChange(e){
-        setAge(e.target.value)
+    function getAge(birthday){
+        const diff = Date.now() - new Date(birthday).getTime();
+        return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25)); 
     }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        // ignore empty inputs
+        if(!name || !birthday ) return
+
+        const newPerson = {
+            id: crypto.randomUUID(),
+            name: name,
+            birthday: birthday,
+        }
+
+        setPeople([...people, newPerson])
+
+        // clear inputs
+        setName("")
+        setBirthday("")
+    }
+    //sort the birthdays
+    const sortedBirthdays = people.sort((a, b) => new Date(a.birthday) - new Date(b.birthday))
+
+
     return (
     <div className="Birthday-reminder">
         <h1>Birthday Reminder</h1>
+        <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Name" value={name} onChange={handleNameChange}/>
         <input type="date" placeholder="Birthday" value={birthday} onChange={handleBirthdayChange}/>
-        <input type="number" placeholder="Age" value={age} onChange={handleAgeChange}/>
-        <button className="submit">Submit</button>
-        <p>Hello {name}</p>
+        <button type="button" onClick={handleSubmit}>Submit</button>
+        </form>
+        <ul>
+            {people.map(sortedBirthdays =>(
+                <li key={sortedBirthdays.id}>{sortedBirthdays.name} - {sortedBirthdays.birthday}   Your are: {getAge(sortedBirthdays.birthday)}</li>
+            ))}
+        </ul>
+        {/* <p>Hello {name}</p>
         <p>Your birthday is {birthday}</p>
-        <p>You are {age} years old</p>
+        <p>You are {age} years old</p> */}
     </div>
     )
 }
